6cdcaad8875ae005135559640d973b0c003b8281
Josphat Kipyegon <josphatkips@gmail.com>
2022-05-14 11:45:16 +0300

    updates

--- resources/views/logs.blade.php
+++ resources/views/logs.blade.php
@@ -1,79 +1,47 @@
 @extends('layouts.main')
 
 
+@section('content')
 
 
-<h1 class="h3 mb-2 text-gray-800">Tables</h1>
-<p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
+
+
+<h1 class="h3 mb-2 text-gray-800">Conversations</h1>
+{{-- <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
     For more information about DataTables, please visit the <a target="_blank"
-        href="https://datatables.net">official DataTables documentation</a>.</p>
+        href="https://datatables.net">official DataTables documentation</a>.</p> --}}
 
 <!-- DataTales Example -->
+<center> <a href="{{url('/')}}" class="btn btn-success">Go back to session</a></center>
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+        <h6 class="m-0 font-weight-bold text-primary">Conversations</h6>
     </div>
     <div class="card-body">
         <div class="table-responsive">
             <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                 <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Position</th>
-                        <th>Office</th>
-                        <th>Age</th>
-                        <th>Start date</th>
-                        <th>Salary</th>
+                        <th>#</th>
+             
+                        <th>Sender</th>
+                        <th>Message</th>
+                        <th>Channel</th>
+                        <th>Date</th>
                     </tr>
                 </thead>
                 <tfoot>
                     <tr>
-                        <th>Name</th>
-                        <th>Position</th>
-                        <th>Office</th>
-                        <th>Age</th>
-                        <th>Start date</th>
-                        <th>Salary</th>
+                        <th>#</th>
+             
+                        <th>Sender</th>
+                        <th>Message</th>
+                        <th>Channel</th>
+                        <th>Date</th>
                     </tr>
                 </tfoot>
                 <tbody>
-                    <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td>61</td>
-                        <td>2011/04/25</td>
-                        <td>$320,800</td>
-                    </tr>
-                    
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
-
-
-
-    <br />
-   <center> <button> <a href="{{url('/')}}">Go back to session</a></button></center>
-    <br />
-    
-
-    <table id="example" class="display" style="width:100%">
-        <thead>
-            <tr>
-                <th>#</th>
-             
-                <th>Sender</th>
-                <th>Message</th>
-                <th>Channel</th>
-                <th>Date</th>
-                {{-- <th>Start date</th>
-                <th>Salary</th> --}}
-            </tr>
-        </thead>
-        <tbody>
-            @php $count=1 @endphp
+                    @php $count=0 @endphp
             @foreach($logs as $log)
             @php
             $count++
@@ -90,25 +58,16 @@
             
 
             @endforeach
-            
-            
-        </tbody>
-        <tfoot>
-            <tr>
-                <th>#</th>
-                <th>Sender</th>
-                <th>Message</th>
-                <th>Channel</th>
-                <th>Date</th>
-            </tr>
-        </tfoot>
-    </table>
-    </table>
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('#example').DataTable();
-        } );
-    </script>
+                    
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+
+   
     
+
+    @stop
