8f923d83ac30b2128e0c4e8f46f66c110b9e8df4
Josphat Kipyegon <josphatkips@gmail.com>
2022-07-01 09:35:41 +0300

    updates

--- resources/views/clients/questions.blade.php
+++ resources/views/clients/questions.blade.php
@@ -8,8 +8,23 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
+
+    
     
     <div class="flex flex-col md:mx-20 py-5">
+        
+        <div>
+            <img src={{asset('assets/img/krb.jpg')}} class="h-28 mx-auto"  />
+        </div>
+        <div class="flex flex-col mx-20">
+            <div class="mx-auto bg-green-500 p-4 rounded-lg text-white text-2xlf font-black">Thank for visiting Kenya Roads Board.</div>
+            <div class="p-5">
+                We care about your experience! We would like you to focus on the satisfaction level that you have experienced in KRB. 
+On a scale of 1 to 5 where 1 IS VERY DISSATISFIED and 5 IS VERY SATISFIED, how satisfied are you with the following areas of customer care at KRB?
+
+            </div>
+
+        </div>
         <div class="flex flex-row">
             <a href="{{url('/admin')}}" class="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Admin</a>
 
@@ -52,13 +67,15 @@
               </div>
 
         </div>
-        <div>
+        <div class="">
+
+            <div class="">
             
                 
                 @foreach ($quiz as $q)
                 <input type="hidden" value="{{$q->id}}" name="question[]" />
-                <div class="text-2xl font-black py-3">{{$q->title}} </div>
-                <div class="flex flex-row flex-wrap md:space-x-4">
+                <div class="md:text-2xl text-yellow-400 font-black py-3">{{$q->title}} </div>
+                <div class="flex flex-row flex-wrap space-x-4 space-y-3">
                     <div class="flex items-center">
                         <input  id="default-radio-2" type="radio" value="1" name="answer[{{$q->id}}]" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" required>
                         <label for="default-radio-2" class="ml-2 text-sm font-medium text-gray-900 ">Very Dissatisfied</label>
@@ -94,6 +111,7 @@
 
                 </div>
             </form>
+        </div>
         </div>
 
     </div>
