d0ff59fa434ddc150083f6f9749c5e0227584887
Josphat Kipyegon <josphatkips@gmail.com>
2022-07-19 10:13:51 +0300

    updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,16 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <div>
+            <center>   <img src={{asset('assets/img/krb.jpg')}} height="150"  /></center> 
+            <br />
+           
+           </div>
+           
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
+                
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
--- resources/views/clients/questions.blade.php
+++ resources/views/clients/questions.blade.php
@@ -17,7 +17,7 @@
             <img src={{asset('assets/img/krb.jpg')}} class="h-28 mx-auto"  />
         </div>
         <div class="flex flex-col mx-20">
-            <div class="mx-auto bg-green-500 p-4 rounded-lg text-white text-2xlf font-black">Thank for visiting Kenya Roads Board.</div>
+            <div class="mx-auto bg-green-700 p-4 rounded-lg text-white text-2xlf font-black">Thank for visiting Kenya Roads Board.</div>
             <div class="p-5">
                 We care about your experience! We would like you to focus on the satisfaction level that you have experienced in KRB. 
 On a scale of 1 to 5 where 1 IS VERY DISSATISFIED and 5 IS VERY SATISFIED, how satisfied are you with the following areas of customer care at KRB?
@@ -74,7 +74,7 @@ On a scale of 1 to 5 where 1 IS VERY DISSATISFIED and 5 IS VERY SATISFIED, how s
                 
                 @foreach ($quiz as $q)
                 <input type="hidden" value="{{$q->id}}" name="question[]" />
-                <div class="md:text-2xl text-green-500 font-black py-3">{{$q->title}} </div>
+                <div class="md:text-2xl text-green-700 font-black py-3">{{$q->title}} </div>
                 <div class="flex flex-row flex-wrap space-x-4 space-y-3">
                     <div class="flex items-center">
                         <input  id="default-radio-2" type="radio" value="1" name="answer[{{$q->id}}]" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" required>
@@ -96,10 +96,7 @@ On a scale of 1 to 5 where 1 IS VERY DISSATISFIED and 5 IS VERY SATISFIED, how s
                         <input  id="default-radio-2" type="radio" value="5" name="answer[{{$q->id}}]"class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                         <label for="default-radio-2" class="ml-2 text-sm font-medium text-gray-900">Very Satisfied</label>
                     </div>
-                    <div class="flex items-center">
-                        <input  id="default-radio-2" type="radio" value="99" name="answer[{{$q->id}}]"class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="default-radio-2" class="ml-2 text-sm font-medium text-gray-900">Don’t Know</label>
-                    </div>
+                   
 
                 </div>
 
