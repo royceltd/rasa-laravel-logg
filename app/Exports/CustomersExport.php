4c7d1020bdefe994af7fc5cf58f8733c8faf05c2
Josphat Kipyegon <josphatkips@gmail.com>
2022-11-23 06:13:15 +0300

    done with export

--- app/Exports/CustomersExport.php
+++ app/Exports/CustomersExport.php
@@ -4,8 +4,12 @@ namespace App\Exports;
 
 use App\Models\Customer;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class CustomersExport implements FromCollection
+class CustomersExport implements  FromCollection, WithColumnFormatting, WithMapping
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +17,21 @@ class CustomersExport implements FromCollection
     public function collection()
     {
         return Customer::all();
+    }
+    public function map($invoice): array
+    {
+        return [
+            $invoice->name,
+            $invoice->email,
+            $invoice->phone,
+            Date::dateTimeToExcel($invoice->created_at)
+        ];
+    }
+    public function columnFormats(): array
+    {
+        return [
+            'D' => NumberFormat::FORMAT_DATE_DDMMYYYY,
+            // 'C' => NumberFormat::FORMAT_CURRENCY_EUR_SIMPLE,
+        ];
     }
 }
